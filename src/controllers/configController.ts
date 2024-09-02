53ebaa09dd2b716a28dd99a550666e58feacc00b
Angel <angelo_stoyanov@abv.bg>
2024-09-02 23:43:05 +0300

    Create Configuration

--- src/controllers/configController.ts
+++ src/controllers/configController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import Configuration from '../models/configuration';
 
 
 export const getConfigurations = async (req: Request, res: Response) => {
@@ -7,7 +8,24 @@ export const getConfigurations = async (req: Request, res: Response) => {
 
 
 export const createConfiguration = async (req: Request, res: Response) => {
-    console.log('createConfiguration')
+    const {
+        name, senderAddress, recipientAddress, minAmount, maxAmount, exactAmount, eventType, blockNumber,
+        gasPrice, gasLimit, status, contractAddress, maxFeePerGas, maxPriorityFeePerGas,
+        gas, nonce, value, transactionType, minTimestamp, maxTimestamp, inputData,
+        accessList, blobVersionedHashes, isActive, monitoringMode, requiredConfirmations
+    } = req.body;
+
+    try {
+        const configuration = await Configuration.create({
+            name, senderAddress, recipientAddress, minAmount, maxAmount, exactAmount, eventType, blockNumber,
+            gasPrice, gasLimit, status, contractAddress, maxFeePerGas, maxPriorityFeePerGas,
+            gas, nonce, value, transactionType, minTimestamp, maxTimestamp, inputData,
+            accessList, blobVersionedHashes, isActive, monitoringMode, requiredConfirmations
+        });
+        res.json(configuration);
+    } catch (error) {
+        res.status(500).json({ message: 'Error creating configuration', error });
+    }
 };
 
 export const updateConfiguration = async (req: Request, res: Response) => {
